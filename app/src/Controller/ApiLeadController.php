378745c7815de2df89c15f399be6b8226e70aa3e
it-aprentiv <it@aprentiv.com>
2022-03-01 11:00:32 +0100

    Formulaire contact

--- app/src/Controller/ApiLeadController.php
+++ app/src/Controller/ApiLeadController.php
@@ -8,26 +8,39 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Lead;
+use App\Entity\LeadNote;
 use App\Form\ApiLeadType;
 use Doctrine\ORM\EntityManagerInterface;
+
 
 class ApiLeadController extends AbstractController
 {
     /**
      * @Route("/api/lead", name="api_lead")
      */
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em,\Swift_Mailer $mailer): Response
     {
         $lead = new Lead;
         $form = $this->createForm(ApiLeadType::class, $lead, ['method' => 'POST']);
         $form->handleRequest($request);
-
+        $page = $request->query->get("page");
         if ($form->isSubmitted() && $form->isValid()) 
-        {
+        {            
+            $commentaire = new LeadNote();
+            $commentaire->setTexteNote($lead->getMessage());
+            $lead->addCommentaire($commentaire);
             $em->persist($lead);
             $em->flush();
-
-            //$this->addFlash('success', 'Lead créé avec succés!');
+            $message = (new \Swift_Message('Hello Email'))
+            ->setFrom($lead->getEmail())
+            ->setTo('abdulah@aprentiv.com')
+            ->setBody(
+               "<h1>".$lead->getMessage()."</h1><br /><p>La page est : ".$page."</p>",
+                'text/html'
+            );
+    
+        $mailer->send($message);
+        $this->addFlash('success', 'Lead créé avec succés!');
             //return $this->redirectToRoute('Fiche_lead_Controller/editLead', array('id' => $lead->getId()));
         }
         $this->viewParams['lead'] = $lead;
